d409267e7eeb70fe18f6cdee3421d2e3b24dc741
Mana Saedan <saedanm@users.noreply.github.com>
2017-06-21 09:48:14 +0700

    Add files via upload

--- FlyME-Server.py
+++ FlyME-Server.py
@@ -41,10 +41,11 @@ if (aircraft.ConnectDevices(False, False) != 0):
     print "Unable connect aircraft devices"
     sys.exit(1)
 
+
 #Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('0.0.0.0', 10000)
 print >>sys.stderr, 'Starting up on %s port %s' % server_address
 sock.bind(server_address)
 #Listen for incoming connections
